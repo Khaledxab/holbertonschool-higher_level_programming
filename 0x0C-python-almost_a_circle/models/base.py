b445cc33a1b6b3762e4f503b17392ea12312f287
Khaledxab <khaledxab@gmail.com>
2022-02-07 21:41:28 +0000

    first test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Base class for all models """
+import json
+
+
+class Base:
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Instantiation method
+        Args:
+            id: id of the instance
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
+    
