c3d817cf6f9e675fcde1303199247ff1fa2874fb
Khaledxab <khaledxab@gmail.com>
2022-04-05 23:39:56 +0200

    SQUARE 1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 const Square1 = require('./5-square.js');
-
 module.exports = class Square extends Square1 {
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
+	charPrint (c) {
+	if (c === undefined) {
+	return this.print();
     } else {
-      for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.height));
-      }
-    }
-  }
-};
+		let A = '';
+		for (let i = 0; i < this.width; i++) {
+		  A += c;
+		}
+		for (let i = 0; i < this.height; i++) {
+		  console.log(A);
+		}
+	  }
+	}
+  };
