ced81f7f8c5ec0e71334d787edf56ef1953d0f1c
khaledxab <khaledxab@gmail.com>
2022-06-19 11:53:18 +0100

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,7 @@
 script takes in an argument and
 displays all values in the states
 where `name` matches the argument
-from  database 
+from  database
 """
 
 import MySQLdb
@@ -12,7 +12,6 @@ from sys import argv
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
-
     c = db.cursor()
     c.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY '{}' \
