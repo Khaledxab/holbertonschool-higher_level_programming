e233045263fe56064de6165111ff02b5242b1b4a
khaledxab <khaledxab@gmail.com>
2022-07-02 21:10:16 +0100

    hbtn stat fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+ a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+def fetch():
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
+
+if __name__ == "__main__":
+	fetch
