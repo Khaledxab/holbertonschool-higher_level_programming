ff2855668cf48049975c25445ca4ba7e3e390c4a
khaledxab <khaledxab@gmail.com>
2022-07-08 02:04:56 +0100

    fix T4 with T6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
   constructor (size) {
-	  super(size, size);
+    super(size, size);
   }
 
   charPrint (c) {
-	  if (c === undefined) {
+    if (c === undefined) {
       this.print();
-	  } else {
+    } else {
       for (let i = 0; i < this.height; i++) {
-		  console.log(c.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
-	  }
+    }
   }
 };
