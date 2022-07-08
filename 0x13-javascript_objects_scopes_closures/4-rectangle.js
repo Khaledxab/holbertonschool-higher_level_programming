f32bc620d7cc178d3f0988556cc31c71ca4ed49e
khaledxab <khaledxab@gmail.com>
2022-07-08 01:55:52 +0100

    redo 4 for 6

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,33 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+	  if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    }
+	  }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+	  let rec = '';
+	  for (let a = 0; a < this.height; a++) {
+      for (let b = 0; b < this.width; b++) {
+		  rec += 'X';
+      }
+      if (i <= this.height - 2) {
+		  rec += '\n';
+      }
+	  }
+	  console.log(rec);
   }
 
   rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
+	  const a = this.height;
+	  this.height = this.width;
+	  this.width = a;
   }
 
   double () {
-    this.width *= 2;
-    this.height *= 2;
+	  this.width *= 2;
+	  this.height *= 2;
   }
 };
