2012b2685c081bd197c73c8ade3c5b973d778d1d
khaledxab <khaledxab@gmail.com>
2022-07-10 01:25:01 +0100

    STATUS check of GET request Try#2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 /** STATUS check of GET request */
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response, body) => {
-  (error) ? console.log(error) : console.log('code:' + response.statusCode);
+request(url, (error, response) => {
+  (error) ? console.log(error) : console.log('code: ' + response.statusCode);
 });
