62f558f641ae4f768ff96723c3e4cb46e586b5ea
khaledxab <khaledxab@gmail.com>
2022-07-13 14:13:31 +0100

    Github commits fetcher

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,14 +7,15 @@ from sys import argv
 def commits():
     repos_name = argv[1]
     owner_name = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repos_name)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner_name, repos_name)
     r = requests.get(url)
     data = r.json()
     try:
         for i in range(10):
             print("{}: {}".format(data[i].get("sha"), data[i].get(
                 "commit").get("author").get("name")))
-    except:
+    except BaseException:
         pass
 
 
