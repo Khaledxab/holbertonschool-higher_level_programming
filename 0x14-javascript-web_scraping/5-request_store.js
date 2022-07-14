b96c71aa0ee94238f3e8bff9992bd70305312e8d
khaledxab <khaledxab@gmail.com>
2022-07-14 01:15:47 +0100

    contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**  gets contents of a webpage*/
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileN = process.argv[3];
+let r;
+request.get(url, (err, respponse, body) => {
+  (err) ? console.log(err) : r = body;
+  fs.writeFile(fileN, r, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
